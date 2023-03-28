29eb0033297057fe9d438a6bb6cefd87736eb1ce
Justin Lau <justinwlau@gmail.com>
2023-03-28 10:52:23 -0700

    tickets are working

--- snakearcade/src/main/java/com/project/snakearcade/controllers/ApiController.java
+++ snakearcade/src/main/java/com/project/snakearcade/controllers/ApiController.java
@@ -3,7 +3,8 @@ package com.project.snakearcade.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.snakearcade.models.User;
@@ -20,14 +21,15 @@ public class ApiController {
 	
 	@GetMapping("/api/users/{id}")
 	public User getOne(@PathVariable("id") Long id) {
+		//User userToGet = userServ.getOne(id);
+		//userToGet.setPasswordConfirm("anything");
 		return userServ.getOne(id);
 	}
 	
 	@PutMapping("/api/users/{id}")
+	@ResponseBody
 	public User update(@PathVariable("id") Long id, 
-			@RequestParam(value="tickets") Integer tickets) {
-		User updateUser = userServ.getOne(id);
-		updateUser.setTickets(tickets);
-		return userServ.update(updateUser);
+			@RequestBody User user) {
+		return userServ.update(user);
 	}
 }
