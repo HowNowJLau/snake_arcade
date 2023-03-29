20d4cf61fad35bc64c1b08b78572ddc715200047
Justin Lau <justinwlau@gmail.com>
2023-03-29 13:57:13 -0700

    collection backend

--- snakearcade/src/main/java/com/project/snakearcade/controllers/RewardController.java
+++ snakearcade/src/main/java/com/project/snakearcade/controllers/RewardController.java
@@ -1,13 +1,20 @@
 package com.project.snakearcade.controllers;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
+import com.project.snakearcade.models.ArchivedReward;
+import com.project.snakearcade.models.Reward;
+import com.project.snakearcade.models.User;
 import com.project.snakearcade.services.ArchivedRewardService;
 import com.project.snakearcade.services.RewardService;
 import com.project.snakearcade.services.UserService;
@@ -38,4 +45,43 @@ public class RewardController {
 		rewardServ.create((Long)session.getAttribute("user_id"), rewardId);
 		return "redirect:/arcade/rewards";
 	}
+	
+	@GetMapping("/arcade/rewards/collection") 
+	public String collection(HttpSession session, Model model) {
+		User user = userServ.getOne((Long)session.getAttribute("user_id"));
+		model.addAttribute("userRewards", user.getRewards());
+		return "/reward/collection.jsp";
+	}
+	
+	@PutMapping("/arcade/rewards/collection/{id}")
+	public String useAvatar(@PathVariable("id") Long id, HttpSession session) {
+		Reward updateReward = rewardServ.getOne(id);
+		disableAllRewards((Long)session.getAttribute("user_id"));
+		updateReward.setIsEnabled(true);
+		rewardServ.update(updateReward);
+		return "redirect:/arcade/rewards/collection";
+	}
+	
+	private void disableAllRewards(Long userId) {
+		User user = userServ.getOne(userId);
+		for (Reward reward : user.getRewards()) {
+			reward.setIsEnabled(false);
+		}
+	}
+	
+	@GetMapping("/arcade/rewards/admin")
+	public String admin(@ModelAttribute("archived_reward") ArchivedReward archived_reward) {
+		return "/reward/admin.jsp";
+	}
+	
+	@PostMapping("/arcade/rewards/admin/create")
+	public String createReward(@Valid @ModelAttribute("archived_reward") ArchivedReward archived_reward,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			return "/reward/admin.jsp";
+		}
+		archivedRewardServ.create(archived_reward);
+		return "redirect:/arcade/rewards/admin";
+	}
+	
 }
---
+++ snakearcade/src/main/webapp/WEB-INF/reward/collection.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
+<%@ page isErrorPage="true" %> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="/css/rewards.css">
+<title>Snake Arcade</title>
+</head>
+<body>
+	<h1>My Collection</h1>
+	<c:forEach var="reward" items="${userRewards}">
+  <div class="one-reward">
+    <p>Image:</p>
+  	<div class="reward-img">
+ 	  <img src="${reward.archivedReward.hyperlink}" alt="avatar" name="${reward.archivedReward.name}"/>
+ 	</div>
+  	<c:choose>
+  		<c:when test="${reward.isEnabled}">
+  			<p>Enabled</p>
+  		</c:when>
+  		<c:otherwise>
+  			<form action="/arcade/rewards/collection/${reward.id}" method="post">
+  			<input type="hidden" name="_method" value="put"/>
+  				<input type="submit" value="Use Avatar"/>
+  			</form>
+  		</c:otherwise>
+  	</c:choose>
+  </div>
+  </c:forEach>
+</body>
+</html>
